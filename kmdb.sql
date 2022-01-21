6d4b151bd2f8e7895e983dd15c2f5f48278c2432
jananikrishnan1 <janani.krishnan@kellogg.northwestern.edu>
2022-01-21 01:40:06 +0000

    WIP final version (need to format + check)

--- kmdb.sql
+++ kmdb.sql
@@ -107,23 +107,7 @@ VALUES
 	('The Dark Knight Rises', 2012, 'PG-13', 'Christopher Nolan');
 
 
--- Batman Begins          Christian Bale        Bruce Wayne
--- Batman Begins          Michael Caine         Alfred
--- Batman Begins          Liam Neeson           Ra's Al Ghul
--- Batman Begins          Katie Holmes          Rachel Dawes
--- Batman Begins          Gary Oldman           Commissioner Gordon
--- The Dark Knight        Christian Bale        Bruce Wayne
--- The Dark Knight        Heath Ledger          Joker
--- The Dark Knight        Aaron Eckhart         Harvey Dent
--- The Dark Knight        Michael Caine         Alfred
--- The Dark Knight        Maggie Gyllenhaal     Rachel Dawes
--- The Dark Knight Rises  Christian Bale        Bruce Wayne
--- The Dark Knight Rises  Gary Oldman           Commissioner Gordon
--- The Dark Knight Rises  Tom Hardy             Bane
--- The Dark Knight Rises  Joseph Gordon-Levitt  John Blake
--- The Dark Knight Rises  Anne Hathaway         Selina Kyle
-
-INSERT INTO top_cast (movie_title, actor, character_name) 
+INSERT INTO top_cast (movie_id, actor, character_name) 
 VALUES
 	(1, 'Christian Bale', 'Bruce Wayne'),
 	(1, 'Michael Caine', 'Alfred'),
@@ -139,7 +123,7 @@ VALUES
     (3, 'Gary Oldman', 'Commissioner Gordon'),
     (3, 'Tom Hardy', 'Bane'),
     (3, 'Joseph Gordon-Levitt', 'John Blake'),
-    (3, 'Anne Hathaway', 'Selina Kyle'),
+    (3, 'Anne Hathaway', 'Selina Kyle');
 
 -- Prints a header for the movies output
 .print "Movies"
